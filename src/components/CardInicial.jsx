6e28e14e8d22415b9d12a4e9cffd914b4acc0d53
GiovannaFerreira12ati <gigita.gita6baby@gmail.com>
2025-04-21 21:38:48 -0300

    mudança da função handleSubmit para o card inicial; alteração da duração do hover do botões

--- src/components/CardInicial.jsx
+++ src/components/CardInicial.jsx
@@ -1,7 +1,15 @@
 import iconStar from "../assets/icon-star.svg"
 import { InputButton } from "./InputButton"
 
-export function CardInicial({ setNotaAvaliacao, handleSubmit}) {
+export function CardInicial({ setNotaAvaliacao, notaAvaliacao, setSubmited}) {
+    function handleSubmit() {
+        if (notaAvaliacao !== 0) {
+          setSubmited(true)
+          return
+        }
+    
+        alert("Please, choose a note!")
+      }
     
 
     return (
@@ -23,7 +31,7 @@ export function CardInicial({ setNotaAvaliacao, handleSubmit}) {
         </div> 
           
 
-        <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange cursor-pointer">Submit</button>
+        <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange cursor-pointer duration-500">Submit</button>
     </div>
     )
 }
